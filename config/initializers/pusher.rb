ed37a20595b26f42976b7623447f75769fc88597
momoticone <bendrissmohamed01@gmail.com>
2017-11-09 18:43:07 +0100

    01

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,3 @@
-Pusher.app_id = ENV["2954"]
-Pusher.key    = ENV["bRbX5YWhuuXp8gSzqDr21kOC9"]
-Pusher.secret = ENV["YhWDfDZ8PvfZzWLk84wI22Xja9UASygVrIhvFPbhjiAVbFjgwk"]
+Pusher.app_id = ENV["pusher_app_id"]
+Pusher.key    = ENV["pusher_key"]
+Pusher.secret = ENV["pusher_secret"]
